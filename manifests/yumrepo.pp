254e69d781abb9464cc9e0b537f7e79bcaa2a782
Rudy YAYON <ryayon@outlook.com>
2016-03-25 10:28:38 +0100

    [YUMREPO] class OK

--- manifests/yumrepo.pp
+++ manifests/yumrepo.pp
@@ -9,8 +9,6 @@ class create_resources::yumrepo (
     noop   => $is_noop,
   }
 
-  notify { "yumrepo is called with ${yumrepo_list}": }
-
   if $yumrepo_list {
     create_resources ( yumrepo, hiera_hash('create_resources::yumrepo::yumrepo_list'), $defaults )
   }
